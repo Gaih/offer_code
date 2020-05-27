cf06c27325bb0e797819e9ff8eda05678390e791
gaixiaotongxue <gaih@outlook.com>
2020-05-28 00:00:22 +0800

    斐波那契数列

--- Code.cpp
+++ Code.cpp
@@ -168,3 +168,20 @@ class CQueue {
 
 
 };
+//斐波那契数列
+int fib(int n) {
+	if (n == 1){
+		return 1;
+	}
+	if (n == 0){
+		return 0;
+	}
+	int t1 =0;
+	int t2 = 1;
+	for(int i=2;i<=n;i++){
+		int t = t2;
+		t2 = (t1+t2) % 1000000007;
+		t1 = t;
+	}
+	return t2;
+}
