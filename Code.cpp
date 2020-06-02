d4d4a6bc710f488b12ad2e35b49edcbb7a378b76
gaixiaotongxue <gaih@outlook.com>
2020-06-02 08:14:30 +0800

    正则

--- Code.cpp
+++ Code.cpp
@@ -386,26 +386,31 @@ ListNode* deleteNode(ListNode* head, int val) {
 }
 //正则表达式匹配
 bool isMatch(string s, string p) {
-    s = "" + s;
-    p = "" + p;
     int m = s.size();
     int n = p.size();
-    bool dp[m][n];
+    vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
     dp[0][0] = true;
-    memset(dp, false, (m+1) * (n+1));
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (p[j] != '*') {
-                if (s[i] == p[j] || p[j] == '.') {
-                    dp[i][j] == dp[i - 1][j - 1];
-                }
-            } else {
-                if (s[i] != p[j - 1] && s[i] != '.') {
-                    dp[i][j] = dp[i][j - 2];
-                } else {
-                    dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j];
-                }
-            }
+    for(int i = 1; i < n; i++) {
+        if (p[i] == '*') {
+            dp[0][i + 1] = dp[0][i - 1];
         }
     }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if (p[j] == '.' || s[i] == p[j]) {
+                dp[i + 1][j + 1] = dp[i][j];
+            } else if (p[j] == '*') {
+                if (s[i] != p[j - 1] && p[j - 1] != '.') {
+                    dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                } else {
+                    dp[i + 1][j + 1] = dp[i][j + 1] || dp[i][j - 1] || dp[i + 1][j - 1];
+                }
+            } else {
+                dp[i + 1][j + 1] = false;
+            }
+        }
+
+    }
+    return dp[m][n];
 }
