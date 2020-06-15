b16319695c1224e961c026a58c3a09f14de0bfc6
gaixiaotongxue <gaih@outlook.com>
2020-06-15 23:59:05 +0800

    礼物的最大价值

--- Code.cpp
+++ Code.cpp
@@ -1165,3 +1165,28 @@ int translateNum(int num) {
     }
     return sum[n - 1];
 }
+//礼物的最大价值
+int maxValue(vector<vector<int>>& grid) {
+    int sum = 0;
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    dp[0][0] = grid[0][0];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+
+            } else if (i == 0 && j > 0) {
+                dp[i][j] = dp[i][j - 1] + grid[i][j];
+
+            } else if(j == 0 && i > 0) {
+                dp[i][0] = dp[i - 1][0] + grid[i][0];
+
+            } else {
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]) + grid[i][j] ;
+            }
+        }
+    }
+    return dp[n - 1][m - 1];
+}
